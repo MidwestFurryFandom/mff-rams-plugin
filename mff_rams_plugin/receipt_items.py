9cb1d379bf4c3c04c58999cddb1ec62096e48f58
Victoria Earl <kitsuta@gmail.com>
2022-09-15 18:23:30 +0200

    Update MFF dealer emails with default costs Fixes the MFF version of the dealer approval email to use the recently-added default_ cost property. Also removes a collision between a named custom property and the default_ cost property.

--- mff_rams_plugin/receipt_items.py
+++ mff_rams_plugin/receipt_items.py
@@ -11,13 +11,13 @@ def table_cost(group):
     if not table_count:
         return
     if group.table_fee:
-        return ("Custom Fee for {}".format(group.tables_repr), group.table_fee)
+        return ("Custom Fee for {}".format(group.tables_repr), group.table_fee * 100)
     
     return ("{} Fee".format(group.tables_repr), c.TABLE_PRICES.get(table_count) * 100)
 
 @cost_calculation.Group
 def power_cost(group):
-    if group.auto_recalc and group.default_power_cost:
-        return ("Tier {} Power Fee".format(group.power), int(group.default_power_cost) * 100)
+    if group.auto_recalc and group.default_power_fee:
+        return ("Tier {} Power Fee".format(group.power), int(group.default_power_fee) * 100)
     elif group.power_fee:
         return ("Custom Fee for Tier {} Power".format(group.power), group.power_fee * 100)
