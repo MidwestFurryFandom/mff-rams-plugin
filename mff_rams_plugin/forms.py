15e499ef3a18aa00ff5bd5049ba733fbb02282a5
Victoria Earl <kitsuta@gmail.com>
2023-07-22 18:27:16 -0400

    Convert customizations for dealer reg Finishes converting all custom dealer-related forms to the new system. Also converts all validations and fixes https://jira.furfest.org/browse/SEI_SD-895 by adding a 1000-character limit for special requests and review notes.

--- mff_rams_plugin/forms.py
+++ mff_rams_plugin/forms.py
@@ -7,18 +7,31 @@ from wtforms.validators import ValidationError, StopValidation
 from uber.config import c
 from uber.forms import AddressForm, MultiCheckbox, MagForm, IntSelect, SwitchInput, DollarInput, HiddenIntField
 from uber.custom_tags import popup_link, format_currency, pluralize, table_prices
-from uber.validations import attendee as attendee_validators
 
 from uber.config import c
 
 @MagForm.form_mixin
 class OtherInfo:
+    promo_code = StringField('Registration Code', description="A discount code or an art show agent code.")
     accessibility_requests = SelectMultipleField('Accommodations Desired', choices=c.ACCESSIBILITY_SERVICE_OPTS, coerce=int, validators=[validators.Optional()], widget=MultiCheckbox())
     other_accessibility_requests = StringField('What other accommodations do you need?')
     fursuiting = BooleanField('I plan on fursuiting at the event.', widget=SwitchInput(), description="This is just to help us prepare; it's okay if your plans change!")
 
     def requested_accessibility_services_label(self):
         return "I have an accessibility request."
+
+@MagForm.form_mixin
+class BadgeExtras:
+    def badge_printed_name_extra_validators(self):
+        return [validators.InputRequired("Please enter a name for your custom-printed badge."),
+                validators.Length(max=30, message="Your printed badge name is too long. \
+                                  Please use less than 30 characters.")]
+    
+    def badge_printed_name_desc(self):
+        return "Badge names have a maximum of 30 characters."
+
+    def badge_type_desc(self):
+        return Markup('<span class="popup"><a href="https://www.furfest.org/registration" target="_blank">What are these?</a></span>')
 
 @MagForm.form_mixin
 class Consents:
@@ -37,23 +50,36 @@ class TableInfo:
         validators.InputRequired("Please select what power level you want, or no power.")
         ], widget=IntSelect())
     power_usage = TextAreaField('Power Usage', description="Please provide a listing of what devices you will be using.")
-    tax_number = StringField('Illinois Business Tax Number',
-                             description="""
-                             If you have an Illinois Business license please provide the number here. Note that this 
-                             number is in the format 1234-5678; it is not your Federal Tax ID or any other Tax ID number 
-                             you may have.""",
-                            render_kw={'pattern': "[0-9]{4}-[0-9]{4}", 'title': "1234-5678"})
-    review_notes = TextAreaField('Review Notes',
-                                 description="""
-                                 Please provide any additional information which you feel could assist in our selection process. 
-                                 This could include additional links other than your website or more detailed merchandise 
-                                 descriptions. If your website includes merchandise which may be in violation of our 
-                                 Intellectual Property rules, please confirm that you will not be bringing this merchandise 
-                                 for sale at MFF.""")
+    tax_number = StringField('Illinois Business Tax Number', validators=[
+        validators.Regexp("^[0-9-]*$", message="Please use only numbers and hyphens for your IBT number.")
+        ], description="""
+                    If you have an Illinois Business license please provide the number here. Note that this 
+                    number is in the format 1234-5678; it is not your Federal Tax ID or any other Tax ID number 
+                    you may have.""", render_kw={'pattern': "[0-9]{4}-[0-9]{4}", 'title': "1234-5678"})
+    review_notes = TextAreaField('Review Notes', validators=[
+        validators.Length(max=1000, message="Review notes cannot be longer than 1000 characters.")
+    ], description="""
+                Please provide any additional information which you feel could assist in our selection process. 
+                This could include additional links other than your website or more detailed merchandise 
+                descriptions. If your website includes merchandise which may be in violation of our 
+                Intellectual Property rules, please confirm that you will not be bringing this merchandise 
+                for sale at MFF.""")
     description = StringField('Merchandise Description', validators=[
         validators.InputRequired("Please provide a description for us to evaluate your submission and use in listings.")
         ], description="This will be used both for dealer selection (if necessary) and in all dealer listings.")
     wares = HiddenField('Wares', validators=[validators.Optional()])
 
+    def special_needs_extra_validators(self):
+        return [validators.Length(max=1000, message="Special requests cannot be longer than 1000 characters.")]
+
     def tables_desc(self):
         return ""
+    
+    def validate_power_usage(form, field):
+        if form.power.data > 0 and not field.data:
+            raise ValidationError("Please provide a list of what powered devices you expect to use.")
+
+@MagForm.form_mixin
+class AdminTableInfo:
+    location = StringField('Table Assignment', render_kw={'placeholder': "Dealer's table location"})
+    power_fee = IntegerField('Power Fee', widget=DollarInput())
