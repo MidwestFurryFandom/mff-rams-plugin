9012c4e0d652df188f3854182107880a0888c396
Victoria Earl <kitsuta@gmail.com>
2024-09-27 20:55:29 -0400

    Update wording for hotel lottery

--- mff_rams_plugin/models.py
+++ mff_rams_plugin/models.py
@@ -295,35 +295,13 @@ class Attendee:
     def has_personalized_badge(self):
         return True
     
-    @property
-    def hotel_lottery_ineligible_reason(self):
-        if not self.is_valid:
-            return "This badge is invalid and cannot enter the hotel lottery."
-        elif self.badge_status in [c.REFUNDED_STATUS, c.NOT_ATTENDING, c.DEFERRED_STATUS]:
-            return f'You cannot enter the hotel lottery with a badge status of "{self.badge_status_label}".'
-        else:
-            return "Please finish registering to enter the hotel lottery."
-            
-
-    @hybrid_property
-    def hotel_lottery_eligible(self):
-        return self.is_valid and self.badge_status not in [c.REFUNDED_STATUS,
-                                                           c.NOT_ATTENDING,
-                                                           c.DEFERRED_STATUS] and not self.placeholder
-
-    @hotel_lottery_eligible.expression
-    def hotel_lottery_eligible(cls):
-        return and_(BaseAttendee.is_valid,
-                    not_(BaseAttendee.badge_status.in_([c.REFUNDED_STATUS, c.NOT_ATTENDING, c.DEFERRED_STATUS])),
-                    BaseAttendee.placeholder == False)
-    
 
 @Session.model_mixin
 class AttendeeAccount:
     @property
-    def hotel_eligible_attendees(self):
-        return [attendee for attendee in self.attendees if attendee.hotel_lottery_eligible]
-    
-    @property
     def hotel_eligible_dealers(self):
         return [attendee for attendee in self.hotel_eligible_attendees if attendee.is_dealer]
+    
+    @property
+    def hotel_eligible_staff(self):
+        return any([a.badge_type == c.STAFF_BADGE or c.STAFF_RIBBON in a.ribbon_ints for a in self.hotel_eligible_attendees])
