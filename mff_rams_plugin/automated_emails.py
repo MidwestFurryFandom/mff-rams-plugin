02f05c3ac040dd9f4b30484263fb0105468a031a
Victoria Earl <kitsuta@gmail.com>
2024-10-04 20:55:21 -0400

    Update lottery phone email text and requirements We actually want to set suite entries to step 1, not 0. Also tweaks the wording for the email.

--- mff_rams_plugin/automated_emails.py
+++ mff_rams_plugin/automated_emails.py
@@ -64,7 +64,7 @@ AutomatedEmailFixture(
     LotteryApplication,
     'Information Needed for {EVENT_NAME} Hotel Lottery',
     'hotel_lottery/lottery_phone.html',
-    lambda a: a.cellphone == '' and a.attendee and a.attendee.cellphone == '' and a.status == c.COMPLETE and a.current_step == 0,
+    lambda a: a.cellphone == '' and a.attendee and a.attendee.cellphone == '' and a.status == c.COMPLETE and a.current_step == (a.last_step - 5),
     sender=c.HOTELS_EMAIL,
     ident='lottery_phone'
 )
