87ca995950f60d72a4e05288a0b62681be0e3cb5
Victoria Earl <kitsuta@gmail.com>
2026-09-19 18:14:59 +0200

    Fix dealer_payment_due property

--- mff_rams_plugin/models.py
+++ mff_rams_plugin/models.py
@@ -174,7 +174,7 @@ class Group:
             else:
                 approval_date = self.approved
 
-            if not c.DEALER_PAYMENT_DUE or approval_date > c.DEALER_PAYMENT_DUE:
+            if not c.DEALER_PAYMENT_DUE or approval_date < c.DEALER_PAYMENT_DUE:
                 return approval_date + timedelta(c.DEALER_PAYMENT_DAYS)
             return c.DEALER_PAYMENT_DUE.astimezone(UTC)
 
